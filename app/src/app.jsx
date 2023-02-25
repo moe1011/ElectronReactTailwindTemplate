fd17ebaab94bb7a2924c3462aaf4de05928f6abf
moe1011 <77053716+moe1011@users.noreply.github.com>
2023-02-25 13:59:25 -0500

    Adjust index.jsx Added app.jsx

---
+++ app/src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div className="App flex bg-gradient-to-r from-blue-700 to-pink-600 h-screen">
+      <div className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl m-auto">
+        Electron, React & Tailwind!
+      </div>
+    </div>
+  );
+}
--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import { App } from "./app";
 import "./index.css";
 
 const container = document.getElementById("target");
@@ -8,10 +8,6 @@ const root = createRoot(container);
 
 root.render(
   <React.Fragment>
-    <div className="App flex bg-gradient-to-r from-blue-700 to-pink-600 h-screen">
-      <div className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl m-auto">
-        Electron, React & Tailwind!
-      </div>
-    </div>
+    <App/>
   </React.Fragment>
 );
