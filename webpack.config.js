7f7b7ed112548c678a35de41c2129fa260cf1e55
moe1011 <77053716+moe1011@users.noreply.github.com>
2023-02-25 19:40:12 -0500

    Added image file loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,15 @@ module.exports = {
                     "css-loader",
                     "postcss-loader"
                 ]
-            }
+            },
+            {
+              test: /\.(png|jpe?g|gif)$/i,
+              use: [
+                {
+                  loader: 'file-loader',
+                },
+              ],
+            },
         ]
     },
     plugins: [
